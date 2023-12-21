cc75724ae2248811692a94be1e05483b724b07f4
nandoProgrammer <nandinhop.costa@gmail.com>
2023-12-21 20:47:37 -0300

    feat: add config routes

--- blog/src/app/app.config.ts
+++ blog/src/app/app.config.ts
@@ -1,8 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { UrlInterceptor } from './core/interceptors/urlInterceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([UrlInterceptor])),
+    { provide: LOCALE_ID, useValue: 'pt-br' },
+  ]
 };
--- blog/src/app/app.routes.ts
+++ blog/src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('../app/blog/blog.component').then(m => m.BlogComponent),
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('../app/blog/components/home/home.component').then(m => m.HomeComponent),
+            },
+            {
+                path: 'article/:idArticle',
+                loadComponent: () => import('../app/blog/components/article/article.component').then(m => m.ArticleComponent),
+            }
+        ]
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () => import('../app/dashboard/dashboard.component').then(m => m.DashboardComponent),
+        children: [
+            {
+                path: 'all-articles',
+                loadComponent: () => import('../app/dashboard/components/list-posts/list-posts.component').then(m => m.ListPostsComponent)
+            },
+            {
+                path: 'new-article',
+                loadComponent: () => import('../app/dashboard/components/form-post/form-post.component').then(m => m.FormPostComponent)
+            },
+            {
+                path: 'edit-post/:idArticle',
+                loadComponent: () => import('../app/dashboard/components/form-post/form-post.component').then(m => m.FormPostComponent)
+            }
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
